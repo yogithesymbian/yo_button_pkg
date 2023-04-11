96de4a5c1a441383fc9e83cf00ed58c6bfb87c0a
yogithesymbian <yogirenbox33@gmail.com>
2023-04-11 08:02:20 +0800

    yo button testing

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -1,7 +1,33 @@
 library flutter_pkg;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class YoButton extends StatelessWidget {
+  Function()? yoPress;
+  final Widget? yoChildWidget;
+  final Color? yoBGColor;
+  var style;
+  YoButton(
+      {Key? key,
+      @required this.yoPress,
+      this.yoChildWidget,
+      this.style,
+      this.yoBGColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: yoPress,
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          primary: Colors.white,
+          backgroundColor: yoBGColor ?? const Color(0xFF2574FF),
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: yoChildWidget ?? const SizedBox());
+  }
 }
